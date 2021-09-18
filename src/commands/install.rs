5d5daaaa8f11a04d0fcb1ca4d0ac48163f754f18
Evie Viau <evie@uwueviee.live>
2021-09-18 18:19:20 -0400

    Add multiple packages support to install

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -1,13 +1,9 @@
+use std::vec;
+
 use crate::util::lock::{create_lock, remove_lock, lock_exists};
 use crate::util::database::{search_for_package};
 
 pub fn install(args: Vec<String>) {
-    sudo::escalate_if_needed().expect("Failed to escalate to root.");
-
-    lock_exists();
-
-    create_lock().expect("Failed to create lock file. (Does /tmp/bulge.funny already exist?)");
-
     if args.len() < 3 {
         eprintln!("Please provide a package to install. (Check bulge --help for usage)");
 
@@ -16,18 +12,34 @@ pub fn install(args: Vec<String>) {
         std::process::exit(1);
     }
 
-    let package: String = args[2].to_lowercase();
-    let repo: String = search_for_package(&package);
+    sudo::escalate_if_needed().expect("Failed to escalate to root.");
 
-    if repo.is_empty() {
-        eprintln!("{} was not found!", package);
+    lock_exists();
 
-        remove_lock().expect("Failed to remove lock?");
+    create_lock().expect("Failed to create lock file. (Does /tmp/bulge.funny already exist?)");
 
-        std::process::exit(1);
+    let packages: Vec<String> = args.clone().drain(2..).collect();
+    let mut repos: Vec<String> = vec![];
+
+    for i in &packages {
+        let repo: String = search_for_package(i);
+    
+        if repo.is_empty() {
+            eprintln!("{} was not found!", i);
+    
+            remove_lock().expect("Failed to remove lock?");
+    
+            std::process::exit(1);
+        }
+
+        repos.push(repo);
     }
 
-    println!("{} was found in {}", package, repo);
+    // TODO: These are debug messages, remove it once function is complete
+    println!("Amount of packages: {}", packages.len());
+    println!("Amount of repos: {}", repos.len());
+    println!("Package vec: {:?}", packages);
+    println!("Repo vec: {:?}", repos);
 
     remove_lock().expect("Failed to remove lock?");
 }
