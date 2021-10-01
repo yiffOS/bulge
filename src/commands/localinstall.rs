add76d2efd3b5ec451de5644a67c7a40baccf00c
Evie Viau <evie@uwueviee.live>
2021-10-01 06:56:09 -0400

    Get package metadata and ask user if they want to install the package

--- src/commands/localinstall.rs
+++ src/commands/localinstall.rs
@@ -1,8 +1,9 @@
 use std::path::Path;
 use std::fs;
+use text_io::read;
 
 use crate::util::lock::{create_lock, remove_lock, lock_exists};
-use crate::util::packaging::fns::{check_if_local_package, decompress_xz};
+use crate::util::packaging::fns::{check_if_package, decompress_xz, decode_pkg_file};
 
 pub fn local_install(args: Vec<String>) {
     if args.len() < 3 {
@@ -24,15 +25,30 @@ pub fn local_install(args: Vec<String>) {
     for i in &packages {
         // Check if i is a valid path and assume it's a file we want to install if it is
         if Path::new(i).exists() {
-            let package = decompress_xz(fs::File::open(i).expect("Failed to read package!"));
-
-            if !check_if_local_package(package) {
+            if !check_if_package(decompress_xz(fs::File::open(i).expect("Failed to read package!"))) {
                 eprintln!("{} is not a valid package!", i);
     
                 remove_lock().expect("Failed to remove lock?");
         
                 std::process::exit(1);
             }
+
+            let mut package_tar = decompress_xz(fs::File::open(i).expect("Failed to read package!"));
+
+            package_tar.unpack(format!("/tmp/bulge/{}", i.split("/").last().unwrap())).unwrap();
+
+            let package = decode_pkg_file(fs::File::open(format!("/tmp/bulge/{}/PKG", i.split("/").last().unwrap()))
+                                                    .expect("Failed to open PKG file!"));
+
+            println!("Installing package {} v{} from {}.", package.name, package.version, &i);
+            println!("Continue? [y/N]");
+            let s: String = read!();
+            if !(s.to_lowercase() == "y".parse::<String>().unwrap()) {
+                println!("Abandoning install!");
+                std::process::exit(1);
+            }
+
+            
         }
     }
 
