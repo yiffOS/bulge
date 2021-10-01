b2914ce9578ecb1510b3bb89250cf1f3ed458e9a
Evie Viau <evie@uwueviee.live>
2021-10-01 03:50:16 -0400

    *Properly* open the file

--- src/commands/localinstall.rs
+++ src/commands/localinstall.rs
@@ -1,5 +1,4 @@
-use std::io::Read;
-use std::{vec, fs};
+use std::fs;
 use std::path::Path;
 use xz2::read::XzDecoder;
 use tar::Archive;
@@ -25,21 +24,24 @@ pub fn local_install(args: Vec<String>) {
 
     for i in &packages {
         // Check if i is a valid path and assume it's a file we want to install if it is
+        println!("Getting package from {}", i);
         if Path::new(i).exists() {
             // Decompress the tar file
-            let mut contents = Vec::new();
-            let file_bytes = fs::read(i).expect("Failed to read package!");
-            let mut decompressor = XzDecoder::new(file_bytes.as_slice());
+            let file_bytes = fs::File::open(i).expect("Failed to read package!");
+            let decompressor = XzDecoder::new(file_bytes);
 
-            decompressor.read(&mut contents).expect("Failed to decompress package!");
+            let mut a = Archive::new(decompressor);
 
-            let mut a = Archive::new(contents.as_slice());
+            println!("Decompressed package, looking for package information.");
 
             // Look for PKG file
             for file in a.entries().unwrap() {
-                let mut file = file.expect("I/O Error!");
+                if  file.unwrap().header().path().unwrap() == Path::new("PKG") {
+                    // If a PKG file is found then this is a valid package
+                    println!("PKG found!");
+                }
 
-                println!("{:?}", file.header().path().unwrap());
+                
             }
         }
     }
