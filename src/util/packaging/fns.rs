ac92b23e2904cd93e205f942a90ac706ab3522f3
Evie Viau <evie@uwueviee.live>
2021-10-01 04:07:17 -0400

    Add function for decompression and package validation

--- src/util/packaging/fns.rs
+++ src/util/packaging/fns.rs
@@ -1,5 +1,27 @@
+use std::fs::File;
+
+use xz2::read::XzDecoder;
+use tar::Archive;
+use std::path::Path;
+
 use crate::util::packaging::structs::LocalPackage;
+
+pub fn decompress_xz(compressed_tar: File) -> Archive<XzDecoder<File>> {
+    return Archive::new(XzDecoder::new(compressed_tar));
+}
 
 pub fn decode_local_package() {
     
+}
+
+pub fn check_if_local_package(mut xztar: Archive<XzDecoder<File>>) -> bool {    
+    // Look for PKG file
+    for file in xztar.entries().unwrap() {
+        if  file.unwrap().header().path().unwrap() == Path::new("PKG") {
+            // If a PKG file is found then this is a valid package
+            return true;
+        }                
+    }
+
+    return false;
 }
