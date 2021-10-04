7eaa9a377ccf7bce2e1a3b315975aabf588dd27f
Evie Viau <evie@uwueviee.live>
2021-10-04 15:34:08 -0400

    Fix adding an extra "," to strings of vecs

--- src/util/macros.rs
+++ src/util/macros.rs
@@ -1,9 +1,13 @@
 /// Converts a vec of strings to a flat string separated by ","
 pub fn vec_to_string(vec: Vec<String>) -> String {
     let mut temp_string: String = String::new();
-    for i in vec {
+    let mut x: usize = 0;
+    for i in &vec {
         temp_string.push_str(&*i);
-        temp_string.push_str(",");
+        if !(x == (&vec.len() - 1)) {
+            temp_string.push_str(",");
+        }
+        x += 1;
     }
     temp_string
 }
