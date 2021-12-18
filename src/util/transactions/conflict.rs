fb4a2f2b2fea8a65b27f333c9fd24d43c9cb11a8
Evie Viau <evie@uwueviee.live>
2021-12-18 16:43:38 -0500

    Implement initial version of package conflictts
    Completes BULGE-4

--- src/util/transactions/conflict.rs
+++ src/util/transactions/conflict.rs
@@ -1,8 +1,15 @@
 use std::path::Path;
+use crate::util::database::fns::get_conflicts;
+use crate::util::database::structs::InstalledPackages;
 
 pub struct ConflictingFiles {
     pub is_conflict: bool,
     pub files: Vec<String>,
+}
+
+pub struct ConflictingPackages {
+    pub is_conflict: bool,
+    pub packages: Vec<InstalledPackages>,
 }
 
 pub fn run_conflict_check(files: &Vec<String>, is_installed: bool, root: String) -> ConflictingFiles {
@@ -16,6 +23,26 @@ pub fn run_conflict_check(files: &Vec<String>, is_installed: bool, root: String)
             conflicting_struct.is_conflict = true;
             conflicting_struct.files.push(format!("{}{}", root.clone(), i.clone()));
         }
+    }
+
+    return conflicting_struct;
+}
+
+pub fn run_conflict_package_check(package: &String) -> ConflictingPackages {
+    let mut conflicting_struct = ConflictingPackages {
+        is_conflict: false,
+        packages: vec![]
+    };
+
+    for i in get_conflicts(package) {
+        if &i.name == package {
+            // Whoops we found a conflict with ourselves, lets skip this one
+            // TODO: Find a better way to search in sqlite so this isn't needed
+            continue;
+        }
+
+        conflicting_struct.is_conflict = true;
+        conflicting_struct.packages.push(i);
     }
 
     return conflicting_struct;
