b8f9805e63a32a159df25fc16f1c847560d01e8e
Evie Viau <evie@uwueviee.live>
2021-12-21 21:14:07 -0500

    Remove old and now unused run_install

--- src/util/packaging/fns.rs
+++ src/util/packaging/fns.rs
@@ -2,7 +2,6 @@ use std::{env, fs::{self, File}, vec};
 use std::path::Path;
 
 use tar::Archive;
-use text_io::read;
 use version_compare::Version;
 use xz2::read::XzDecoder;
 
@@ -30,128 +29,6 @@ pub fn check_if_package(mut xztar: Archive<XzDecoder<File>>) -> bool {
     }
 
     return false;
-}
-
-pub fn run_install(file: File, tmp_path: &str, source: Source) {
-    let mut package_tar = decompress_xz(file);
-
-    package_tar.unpack(format!("{}/tmp/bulge/{}", get_root(), tmp_path)).unwrap();
-
-    let package = decode_pkg_file(fs::File::open(format!("{}/tmp/bulge/{}/PKG", get_root(), tmp_path))
-        .expect("Failed to open PKG file!"));
-
-    // TODO: Check for conflicting packages
-    println!("Looking for conflicting packages...");
-
-    // TODO: Check for dependencies
-    println!("Looking for dependencies...");
-
-    println!();
-    println!("Installing package {} v{} from {}.", &package.name, &package.version, &source.name);
-
-    // Ask the user if they'd like to install the specified package
-    println!("Continue? [y/N]");
-    
-    let s: String = read!();
-    if !(s.to_lowercase() == "y".parse::<String>().unwrap()) {
-        println!("Abandoning install!");
-        std::process::exit(1);
-    }
-
-    // Check if package is already installed
-    let installed_pkg = get_installed_package(&package.name);
-    if installed_pkg.is_ok() {
-        // Check if this is a downgrade
-        if Version::from(&package.version) > Version::from(&installed_pkg.as_ref().unwrap().version) {
-            let installed_pkg = get_installed_package(&package.name); // Result doesn't have copy
-
-            // Ask the user if they'd like to still install the specified package
-            println!("This will result in a downgrade as {} v{} is already installed!", &package.name, &installed_pkg.unwrap().version);
-
-            println!("Continue? [y/N]");
-            let s: String = read!();
-            if !(s.to_lowercase() == "y".parse::<String>().unwrap()) {
-                println!("Abandoning install!");
-                std::process::exit(1);
-            }
-        }
-
-        println!("Warning: {} is already installed, reinstalling...", &package.name);
-    }
-
-    // Decompress data
-    let mut data_tar_files = decompress_xz(fs::File::open(format!("{}/tmp/bulge/{}/data.tar.xz", get_root(), tmp_path)).expect("Failed to read package!"));
-
-    // Calculate files to be installed and extract to temp folder
-    let mut files: Vec<String> = vec![];
-
-    data_tar_files.entries()
-        .expect("IO Error!")
-        .filter_map(|e| e.ok())
-        .for_each(|x| {
-            if !x.header().path().unwrap().to_string_lossy().ends_with("/") {
-                files.push(format!("/{}" ,x.header().path().unwrap().to_string_lossy().to_string()));
-            }
-        });
-
-    println!();
-    println!("Looking for conflicting files...");
-    let conflicting = run_conflict_check(&files, installed_pkg.is_ok(), get_root());
-
-    if conflicting.is_conflict {
-        eprintln!("Package files already exist on the file system!");
-
-        println!("Continue? THIS WILL DELETE FILES! [y/N]");
-        let s: String = read!();
-
-        if !(s.to_lowercase() == "y".parse::<String>().unwrap()) {
-            println!("Abandoning install!");
-
-            remove_lock().expect("Failed to remove lock?");
-
-            std::process::exit(1);
-        } else {
-            println!("Continuing install!");
-
-            for i in conflicting.files {
-                println!("Removing {}", i);
-                fs::remove_file(i).expect("Failed to delete file!");
-            }
-        }
-    }
-
-    //Add package to database
-    add_package_to_installed(NewPackage { 
-        name: package.name.clone(), 
-        groups: package.groups, 
-        version: package.version.clone(), 
-        epoch: package.epoch, 
-        installed_files: files,
-        provides: string_to_vec(package.provides),
-        conflicts: string_to_vec(package.conflicts),
-        dependencies: string_to_vec(package.depends),
-    }, source);
-
-    println!("Decompressing files...");
-    
-    // Open data tar for extraction
-    let mut data_tar = decompress_xz(fs::File::open(format!("{}/tmp/bulge/{}/data.tar.xz", get_root(), tmp_path)).expect("Failed to read package!"));
-
-    println!("Unpacking files...");
-
-    // Extract files onto root
-    data_tar.set_preserve_permissions(true);
-    data_tar.set_unpack_xattrs(true);
-
-    data_tar
-        .unpack(get_root() + "/")
-        .expect("Extraction error!");
-
-    // Clean files up
-    fs::remove_dir_all(format!("{}/tmp/bulge/{}", get_root(), tmp_path)).expect("Failed to delete temp path!");
-    
-    println!();
-    println!("Installed {} v{}!", &package.name, &package.version);
 }
 
 pub fn run_remove(package: &String) {
