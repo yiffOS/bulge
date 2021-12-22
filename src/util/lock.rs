a95724b1170f31c5e47cb10f74fe08127243840f
Evie Viau <evie@uwueviee.live>
2021-12-21 21:13:37 -0500

    Use new macro for prompting to deleting lock file

--- src/util/lock.rs
+++ src/util/lock.rs
@@ -1,6 +1,6 @@
 use std::{fs, fs::File};
 use std::path::Path;
-use text_io::read;
+use crate::util::macros::continue_prompt;
 
 /// Creates a lock file indicating that bulge is open
 pub fn create_lock() -> std::io::Result<()> {
@@ -23,9 +23,8 @@ pub fn check_lock() -> bool {
 pub fn lock_exists() {
     if check_lock() {
         println!("An instance of bulge is already running.");
-        println!("Delete lock file? (Only do this when the other process is frozen) [y/N]");
-        let s: String = read!();
-        if s.to_lowercase() == "y".parse::<String>().unwrap() {
+        println!("Delete lock file? (Only do this when the other process is frozen)");
+        if continue_prompt() {
             remove_lock().expect("Failed to delete lock file.");
         } else {
             std::process::exit(1);
