9e9a5f14303db6f37631a80ccfa1daae5a5ced19
Evie Viau <evie@eviee.gay>
2022-04-01 22:07:03 +0000

    Fix BULGE-17 by checking epoch as well as version for reinstall message

--- src/util/transactions/install.rs
+++ src/util/transactions/install.rs
@@ -44,7 +44,7 @@ pub fn run_install(install: InstallTransaction, file: File) {
                 remove_lock().expect("Failed to remove lock!");
                 std::process::exit(1);
             }
-        } else if Version::from(&package.version) == Version::from(&installed_pkg.as_ref().unwrap().version) {
+        } else if (Version::from(&package.version) == Version::from(&installed_pkg.as_ref().unwrap().version)) && (&package.epoch == &installed_pkg.as_ref().unwrap().epoch) {
             println!("> Warning: {} is already installed, reinstalling...", &package.name);
         }
 
